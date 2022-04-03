764fdf9deeb919db300bdf43a232ff741ba01fe6
svenwas3f <sven.waser@venusnet.ch>
2022-04-03 20:46:00 +0200

    Inital

---
+++ report.php
@@ -0,0 +1,8 @@
+<?php
+class quiz_exportresults_report extends quiz_default_report {
+    public function display($cm, $course, $quiz) {
+        // Generate and display the report, or
+        // other functionality.
+    }
+}
+?>
