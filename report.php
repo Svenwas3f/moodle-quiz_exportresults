626e32599e29a0bc7ecc77d0b69c7b9b49da3f4e
svenwas3f <sven.waser@venusnet.ch>
2022-04-05 22:34:43 +0200

    Changed quiz and course

--- report.php
+++ report.php
@@ -25,7 +25,7 @@
 
 
 class quiz_exportresults_report extends quiz_default_report {
-  public function display($cm, $course, $quiz) {
+  public function display($cm, $quiz, $course) {
     // Display page
     $this->print_header_and_tabs($cm, $course, $quiz, 'quiz_exportresults');
 
