deeb711f8b75541a31b56ec1ee4d46b7201f2899
svenwas3f <sven.waser@venusnet.ch>
2022-04-11 20:37:59 +0200

    Update

--- report.php
+++ report.php
@@ -20,6 +20,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+require_once($CFG->dirroot . '/mod/quiz/report/exportresults/exportresults_form.php');
+
 class quiz_exportresults_report extends quiz_default_report {
   /**
    * Function to display report
@@ -29,156 +31,179 @@ class quiz_exportresults_report extends quiz_default_report {
    */
   public function display($quiz, $cm, $course) {
     // Get database
-    global $DB, $context;
+    global $DB, $PAGE, $context;
 
-    // Check if groups activated
-    if(groups_get_course_groupmode($course) != 0) {
-      $groups = groups_get_all_groups($course->id); // Get all groups / groups_get_user_groups
-    }else {
-      $groups = array((object) array("id" => 0, "courseid" => $course->id, "name" => $course->fullname) ); // No groups activated
-    }
+    // Initialise form
+    $mform = new quiz_exportresults_form($PAGE->url);
 
-    /////////////////////////// Generate export ///////////////////////////
-    $path = make_request_directory() . "/sexporsubmissionplugin/export/";
-    $filename = 'export.zip';
-    mkdir($path, 0777, true); // Generate temp path
-
-    $export = new ZipArchive();
-    $export->open($path . $filename, ZipArchive::CREATE); // Initialise zip
-
-    // Loop groups
-    foreach($groups as $group) {
-      // Add folder to export zip
-      $export->addEmptyDir($group->name);
-
-      // Handle attempts
-      if(groups_get_course_groupmode($course)) {
-        $members = groups_get_members($group->id); // Get members of group
+    // Check if export requested
+    if($data = $mform->get_data()) {
+      // Check if groups activated
+      if(groups_get_course_groupmode($course) == false || empty($data->groups)) {
+        $groups = array((object) array("id" => 0, "courseid" => $course->id, "name" => $course->fullname) ); // No groups activated
       }else {
-        $members = get_enrolled_users($context); // Get members of course
+        $groups = $data->groups;
       }
 
-      foreach($members as $member) {
-        // Options: highest grade, first attempt, last attempts, all attempts
-        switch('all') {
-          case 'highest': // Highest attempt
-            $params['quiz'] = $quiz->id; // Quiz ID
-            $params['userid'] = $member->id; // User ID
+      /////////////////////////// Generate export ///////////////////////////
+      $path = make_request_directory() . "/sexporsubmissionplugin/export/";
+      $filename = 'export.zip';
+      mkdir($path, 0777, true); // Generate temp path
 
-            $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'sumgrades DESC', '*', 0, 1); // Request attempts
-            break;
-          case 'first': // first attempt
-            $params['quiz'] = $quiz->id; // Quiz ID
-            $params['userid'] = $member->id; // User ID
+      $export = new ZipArchive();
+      $export->open($path . $filename, ZipArchive::CREATE); // Initialise zip
 
-            $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'attempt ASC', '*', 0, 1); // Request attempts
-            break;
-          case 'last': // Last attempt
-            $params['quiz'] = $quiz->id; // Quiz ID
-            $params['userid'] = $member->id; // User ID
+      // Loop groups
+      foreach($groups as $group) {
+        // Add folder to export zip
+        $export->addEmptyDir($group->name);
 
-            $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'attempt DESC', '*', 0, 1); // Request attempts
-            break;
-          case 'all': // All attempts
-          default:
-            $params['quiz'] = $quiz->id; // Quiz ID
-            $params['userid'] = $member->id; // User ID
-
-            $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'attempt DESC'); // Request attempts
-            break;
+        // Handle attempts
+        if(groups_get_course_groupmode($course) == false || empty($data->groups)) {
+          $members = get_enrolled_users($context); // Get members of course
+        }else {
+          $members = groups_get_members($group->id); // Get members of group
         }
 
-        // Get acctual value
-        foreach($attempts as $attempt) {
-          $params['id'] = $attempt->id; // Attempt ID
-          $attempt_value = $DB->get_records_select('question_attempts', 'id=:id', $params, 'timemodified DESC'); // Request attempts
+        foreach($members as $member) {
+          // Options: highest grade, first attempt, last attempts, all attempts
+          switch($data->attempts) {
+            case 'highest': // Highest attempt
+              $params['quiz'] = $quiz->id; // Quiz ID
+              $params['userid'] = $member->id; // User ID
 
-          // Prepare values for odt
-          $content[0]["val"][0]["val"][0]["val"][0]["name"] = 'text:p';
-          $content[0]["val"][0]["val"][0]["val"][0]["att"] = array('text:style-name' => 'Standard');
-          $content[0]["val"][0]["val"][0]["val"][0]["val"] = array_values($attempt_value)[0]->responsesummary . "Das ist ein langer text um den Zeileunterschied zu erkennen. Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla BlaBla Bla BlaBla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla BlaBla Bla Bla";
+              $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'sumgrades DESC', '*', 0, 1); // Request attempts
+              break;
+            case 'first': // first attempt
+              $params['quiz'] = $quiz->id; // Quiz ID
+              $params['userid'] = $member->id; // User ID
 
-          $meta[0]["val"][0]["val"][0]["name"] = 'meta:initial-creator';
-          $meta[0]["val"][0]["val"][0]["val"] = 'Moodle Exportresults Plugin';
-          $meta[0]["val"][0]["val"][1]["name"] = 'meta:creation-date';
-          $meta[0]["val"][0]["val"][1]["val"] = date("Y-m-d\TH:i:sp");
-          $meta[0]["val"][0]["val"][3]["name"] = 'meta:creator';
-          $meta[0]["val"][0]["val"][3]["val"] = 'Moodle Exportresults Plugin';
+              $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'attempt ASC', '*', 0, 1); // Request attempts
+              break;
+            case 'last': // Last attempt
+              $params['quiz'] = $quiz->id; // Quiz ID
+              $params['userid'] = $member->id; // User ID
 
-          $styles[0]["val"][0]["name"] = 'office:font-face-decls';
-          $styles[0]["val"][0]["val"][0]["name"] = 'style:font-face';
-          $styles[0]["val"][0]["val"][0]["att"]["style:name"] = 'Times New Roman';
-          $styles[0]["val"][0]["val"][0]["att"]["svg:font-family"] = '&apos;Times New Roman&apos;';
-          $styles[0]["val"][0]["val"][1]["name"] = 'style:font-face';
-          $styles[0]["val"][0]["val"][1]["att"]["style:name"] = 'Arial';
-          $styles[0]["val"][0]["val"][1]["att"]["svg:font-family"] = 'Arial';
+              $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'attempt DESC', '*', 0, 1); // Request attempts
+              break;
+            case 'all': // All attempts
+            default:
+              $params['quiz'] = $quiz->id; // Quiz ID
+              $params['userid'] = $member->id; // User ID
 
-          $styles[0]["val"][1]["name"] = 'office:styles';
-          $styles[0]["val"][1]["val"][0]["name"] = 'style:default-style';
-          $styles[0]["val"][1]["val"][0]["att"]["style:family"] = 'paragraph';
-          $styles[0]["val"][1]["val"][0]["val"][0]["name"] = 'style:text-properties';
-          $styles[0]["val"][1]["val"][0]["val"][0]["att"]['fo:font-size'] = '20pt'; // Fontsize
-          $styles[0]["val"][1]["val"][0]["val"][0]["att"]['style:font-name'] = 'Arial'; // Font family
-          $styles[0]["val"][1]["val"][0]["val"][0]["name"] = 'style:paragraph-properties';
-          $styles[0]["val"][1]["val"][0]["val"][0]["att"]['fo:line-height'] = '180%'; // line height
+              $attempts = $DB->get_records_select('quiz_attempts', 'quiz=:quiz AND userid=:userid', $params, 'attempt DESC'); // Request attempts
+              break;
+          }
 
-          $styles[0]["val"][2]["name"] = 'office:automatic-styles';
-          $styles[0]["val"][2]["val"][0]["name"] = 'style:page-layout';
-          $styles[0]["val"][2]["val"][0]["att"]["style:name"] = 'mdl1';
-          $styles[0]["val"][2]["val"][0]["val"][0]["name"] = 'style:page-layout-properties';
-          $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-bottm"] = '5cm'; // Margin
-          $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-top"] = '5cm'; // Margin
-          $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-left"] = '5cm'; // Margin
-          $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-right"] = '5cm'; // Margin
+          // Get acctual value
+          foreach($attempts as $attempt) {
+            $params['id'] = $attempt->id; // Attempt ID
+            $questions = $DB->get_records_select('question_attempts', 'id=:id', $params, 'timemodified DESC'); // Request attempts
 
-          $styles[0]["val"][3]["name"] = 'office:master-styles';
-          $styles[0]["val"][3]["val"][0]["name"] = 'style:master-page';
-          $styles[0]["val"][3]["val"][0]["att"]["style:name"] = 'Standard';
-          $styles[0]["val"][3]["val"][0]["att"]["style:page-layout-name"] = 'mdl1';
+            // Prepare values for odt
+            $count = 0;
+            foreach($questions as $question) {
+              if($data->questions == 1) {
+                $content[0]["val"][0]["val"][0]["val"][$count]["name"] = 'text:p';
+                $content[0]["val"][0]["val"][0]["val"][$count]["att"] = array('text:style-name' => 'Standard');
+                $content[0]["val"][0]["val"][0]["val"][$count]["val"] = $question->questionsummary;
+                $count++;
 
-          // Generate odt and add to export
-          $odt = $this->odt($content, $meta, array(), $styles);
+                $content[0]["val"][0]["val"][0]["val"][$count]["name"] = 'text:p';
+                $content[0]["val"][0]["val"][0]["val"][$count]["att"] = array('text:style-name' => 'Standard');
+                $content[0]["val"][0]["val"][0]["val"][$count]["val"] = $question->responsesummary;
+                $count++;
+              }else {
+                $content[0]["val"][0]["val"][0]["val"][$count]["name"] = 'text:p';
+                $content[0]["val"][0]["val"][0]["val"][$count]["att"] = array('text:style-name' => 'Standard');
+                $content[0]["val"][0]["val"][0]["val"][$count]["val"] = $question->responsesummary;
+                $count++;
+              }
+            }
 
-          $pattern = '/[^A-Za-z0-9-_]/'; // Pattern to secure filename
-          $export->addFile($odt, $group->name . "/" . preg_replace($pattern, '', $member->username) . "_" . preg_replace($pattern, '', $member->firstname) . "_" . preg_replace($pattern, '', $member->lastname) . ".odt");
+            $meta[0]["val"][0]["val"][0]["name"] = 'meta:initial-creator';
+            $meta[0]["val"][0]["val"][0]["val"] = 'Moodle Exportresults Plugin';
+            $meta[0]["val"][0]["val"][1]["name"] = 'meta:creation-date';
+            $meta[0]["val"][0]["val"][1]["val"] = date("Y-m-d\TH:i:sp");
+            $meta[0]["val"][0]["val"][3]["name"] = 'meta:creator';
+            $meta[0]["val"][0]["val"][3]["val"] = 'Moodle Exportresults Plugin';
+
+            $styles[0]["val"][0]["name"] = 'office:font-face-decls';
+            $styles[0]["val"][0]["val"][0]["name"] = 'style:font-face';
+            $styles[0]["val"][0]["val"][0]["att"]["style:name"] = 'Times New Roman';
+            $styles[0]["val"][0]["val"][0]["att"]["svg:font-family"] = '&apos;Times New Roman&apos;';
+            $styles[0]["val"][0]["val"][1]["name"] = 'style:font-face';
+            $styles[0]["val"][0]["val"][1]["att"]["style:name"] = 'Arial';
+            $styles[0]["val"][0]["val"][1]["att"]["svg:font-family"] = 'Arial';
+
+            $styles[0]["val"][1]["name"] = 'office:styles';
+            $styles[0]["val"][1]["val"][0]["name"] = 'style:default-style';
+            $styles[0]["val"][1]["val"][0]["att"]["style:family"] = 'paragraph';
+            $styles[0]["val"][1]["val"][0]["val"][0]["name"] = 'style:text-properties';
+            $styles[0]["val"][1]["val"][0]["val"][0]["att"]['fo:font-size'] = '20pt'; // Fontsize
+            $styles[0]["val"][1]["val"][0]["val"][0]["att"]['style:font-name'] = 'Arial'; // Font family
+            $styles[0]["val"][1]["val"][0]["val"][0]["name"] = 'style:paragraph-properties';
+            $styles[0]["val"][1]["val"][0]["val"][0]["att"]['fo:line-height'] = '180%'; // line height
+
+            $styles[0]["val"][2]["name"] = 'office:automatic-styles';
+            $styles[0]["val"][2]["val"][0]["name"] = 'style:page-layout';
+            $styles[0]["val"][2]["val"][0]["att"]["style:name"] = 'mdl1';
+            $styles[0]["val"][2]["val"][0]["val"][0]["name"] = 'style:page-layout-properties';
+            $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-bottm"] = '5cm'; // Margin
+            $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-top"] = '5cm'; // Margin
+            $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-left"] = '5cm'; // Margin
+            $styles[0]["val"][2]["val"][0]["val"][0]["att"]["fo:margin-right"] = '5cm'; // Margin
+
+            $styles[0]["val"][3]["name"] = 'office:master-styles';
+            $styles[0]["val"][3]["val"][0]["name"] = 'style:master-page';
+            $styles[0]["val"][3]["val"][0]["att"]["style:name"] = 'Standard';
+            $styles[0]["val"][3]["val"][0]["att"]["style:page-layout-name"] = 'mdl1';
+
+            // Generate odt and add to export
+            $odt = $this->odt($content, $meta, array(), $styles);
+
+            $pattern = '/[^A-Za-z0-9-_]/'; // Pattern to secure filename
+            $export->addFile($odt, $group->name . "/" . preg_replace($pattern, '', $member->username) . "_" . preg_replace($pattern, '', $member->firstname) . "_" . preg_replace($pattern, '', $member->lastname) . ".odt");
+          }
         }
       }
+
+      // Convert to zip and enable downlod
+      $export->close();
+
+      // Copy odt into download Zip
+      $fs = get_file_storage();
+
+      // Prepare file record object
+      $fileinfo = array(
+          'contextid' => $context->id,         // ID of context
+          'component' => 'quiz_exportresults', // usually = table name
+          'filearea' => 'export',              // usually = table name
+          'itemid' => $quiz->id,               // usually = ID of row in table
+          'filepath' => '/',                   // any path beginning and ending in /
+          'filename' => 'export.zip');         // any filename
+
+      if($fs->file_exists($fileinfo["contextid"], $fileinfo["component"], $fileinfo["filearea"], $fileinfo["itemid"], $fileinfo["filepath"], $fileinfo["filename"])) {
+        $fs->get_file($fileinfo['contextid'], $fileinfo['component'], $fileinfo['filearea'], $fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename'])->delete(); // Remove file
+      }
+
+      // Create file in File API
+      $exportfile = $fs->create_file_from_pathname($fileinfo, $path . $filename);
+
+      // Serve to user
+      send_stored_file($exportfile, 86400, 0, true);
     }
-
-    // Convert to zip and enable downlod
-    $export->close();
-
-    // Copy odt into download Zip
-    $fs = get_file_storage();
-
-    // Prepare file record object
-    $fileinfo = array(
-        'contextid' => $context->id, // ID of context
-        'component' => 'quiz_exportresults',     // usually = table name
-        'filearea' => 'export',     // usually = table name
-        'itemid' => $quiz->id,               // usually = ID of row in table
-        'filepath' => '/',           // any path beginning and ending in /
-        'filename' => 'export.zip'); // any filename
-
-    if($fs->file_exists($fileinfo["contextid"], $fileinfo["component"], $fileinfo["filearea"], $fileinfo["itemid"], $fileinfo["filepath"], $fileinfo["filename"])) {
-      $fs->get_file($fileinfo['contextid'], $fileinfo['component'], $fileinfo['filearea'], $fileinfo['itemid'], $fileinfo['filepath'], $fileinfo['filename'])->delete(); // Remove file
-    }
-
-    // Create file in File API
-    $exportfile = $fs->create_file_from_pathname($fileinfo, $path . $filename);
-
-    // Serve to user
-    // send_stored_file($exportfile, 86400, 0, true);
 
     // Display page
     $this->print_header_and_tabs($cm, $course, $quiz, 'quiz_exportresults');
 
-
-    if(quiz_has_attempts($quiz->id)) {
-      // Display form
-    }else {
-      // Display message
+    // Display notification
+    if(!quiz_has_attempts($quiz->id)) {
+      \core\notification::error( get_string('exportresults_no_attempts', 'quiz_exportresults') );
     }
+
+    // Display form
+    $mform->display();
   }
 
   /**
