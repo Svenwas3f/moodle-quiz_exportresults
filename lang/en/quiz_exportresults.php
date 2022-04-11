7d966b091659205074cd73dcca2f06f1ae8a9021
svenwas3f <sven.waser@venusnet.ch>
2022-04-11 20:38:10 +0200

    Language added

--- lang/en/quiz_exportresults.php
+++ lang/en/quiz_exportresults.php
@@ -15,14 +15,34 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'quiz_downloadsubmissions', language 'en'
+ * Strings for component 'quiz_exportresults', language 'en'
  *
- * @package   quiz_downloadsubmissions
- * @copyright 2017 IIT Bombay
- * @author    Kashmira Nagwekar
+ * @package   quiz_exportresults
+ * @copyright 2022, Sven Waser <sven.waser@ksso.ch>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 $string["pluginname"] = "exportresults";
 $string["reportname"] = "exportresults";
 $string["namereport"] = "exportresults";
 $string["exportresults"] = "Export Results";
+
+$string["exportresults_exportoptions_header"] = "General exportoptions";
+$string["exportresults_exportoptions_select_info"] = "Export attempts";
+$string["exportresults_exportoptions_option_highest_grade"] = "Highest grade";
+$string["exportresults_exportoptions_option_first_attempt"] = "First attempts";
+$string["exportresults_exportoptions_option_last_attempt"] = "Last attempts";
+$string["exportresults_exportoptions_option_all_attempts"] = "All attempts";
+$string["exportresults_exportoptions_checkbox_question"] = "Include questiontext in export";
+$string["exportresults_exportoptions_checkbox_question_label"] = "Default: Yes";
+$string["exportresults_exportoptions_select_groups"] = 'Select groups (CRTL for multiple)';
+
+$string["exportresults_fileoptions_header"] = 'File preferences';
+$string["exportresults_fileoption_margintop"] = 'Margin top (cm)';
+$string["exportresults_fileoption_marginright"] = 'Margin right (cm)';
+$string["exportresults_fileoption_marginbottom"] = 'Margin bottom (cm)';
+$string["exportresults_fileoption_marginleft"] = 'Margin left (cm)';
+$string["exportresults_fileoption_fontsize"] = 'Font size (pt)';
+$string["exportresults_fileoption_lineheight"] = 'Line height';
+$string["exportresutls_submit"] = "Export";
+
+$string["exportresults_no_attempts"] = 'No attempts found. Empty export.';
